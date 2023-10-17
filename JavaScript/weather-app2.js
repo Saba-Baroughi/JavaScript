cc4cf43878f93aff0c02dc804049ef8f61ce134e
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-10-17 11:42:41 +0100

    WeatherApp.2way

--- JavaScript/weather-app2.js
+++ JavaScript/weather-app2.js
@@ -1,61 +1,55 @@
-let citiesData = {
-  tehran: {
-    city: "Tehran",
-    temp: 12,
-    weather: "Sunny",
-    humidity: 23,
-    windSpeed: 32,
-  },
-  shiraz: {
+let citiesData = [
+  // the difference of first way is that here we remove key (tehran:)
+  { city: "Tehran", temp: 12, weather: "Sunny", humidity: 23, windSpeed: 32 },
+  {
     city: "Shiraz",
     temp: 9,
     weather: "windy",
     humidity: 12,
     windSpeed: 14,
   },
-  tabriz: {
+  {
     city: "Tabriz",
     temp: 1,
     weather: "rainy",
     humidity: 43,
     windSpeed: 12,
   },
-  mashhad: {
+  {
     city: "Mashhad",
     temp: 16,
     weather: "snowy",
     humidity: 7,
     windSpeed: 24,
   },
-  esfahan: {
+  {
     city: "Esfahan",
     temp: 23,
     weather: "Sunny",
     humidity: 15,
     windSpeed: 18,
   },
-};
-
+];
 let $ = document;
-let searchBtn = $.getElementById("search");
 let searchBar = $.querySelector(".search-bar");
-
+let searchBtn = $.getElementById("search");
 searchBtn.addEventListener("click", function () {
-  let searchBarValue = searchBar.value; // tabriz//    instead of innerHTML==> value
-  let mainCityDatas = citiesData[searchBarValue];
-
+  let searchBarValue = searchBar.value; //instead of innerHTML-->value
+  let mainCityDatas = citiesData.find(function (item) {
+    return item.city === searchBarValue;
+  });
   console.log(mainCityDatas);
   if (mainCityDatas) {
-    //it is an object and Boolean({})=truthy value
     $.querySelector(".weather").classList.remove("loading");
-    $.querySelector(".city").innerHTML = "Wheather in" + mainCityDatas.city;
+    $.querySelector(".city").innerHTML = "Weather in " + mainCityDatas.city;
     $.querySelector(".temp").innerHTML = mainCityDatas.temp + "°C";
     $.querySelector(".description").innerHTML = mainCityDatas.weather;
-    $.querySelector(".wind").innerHTML =
-      "wind speed:" + mainCityDatas.wind + "km/h";
     $.querySelector(".humidity").innerHTML =
       "Humidity: " + mainCityDatas.humidity;
+    $.querySelector(".wind").innerHTML =
+      "Wind speed: " + mainCityDatas.windSpeed + "km/h";
   } else {
-    alert("Plaese enter the valid city");
+    alert("Enter tha valid city");
   }
 });
+0;
