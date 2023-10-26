fdc28e2260d58163e8a030a1c65c0808a6469217
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-10-26 16:29:30 +0100

    practice

--- practiceSheet.js
+++ practiceSheet.js
@@ -1,6 +1,35 @@
-let listitems = document.querySelectorAll(".list-item");
-listitems.forEach(function (li) {
-  li.addEventListener("click", function (event) {
-    console.log(event.target.parentElement);
+let $ = document;
+let inputElem = $.querySelector("input");
+let todoUlElem = $.querySelector("ul");
+let addTodoForm = $.querySelector(".add");
+
+function addNewTodo(newTodoValue) {
+  let newTodoTrash = $.createElement("i");
+  let newTodoTitleSpan = $.createElement("span");
+  let newTodoLi = $.createElement("li");
+  newTodoLi.className =
+    "list-group-item d-flex justify-content-between align-items-center";
+  newTodoTitleSpan.innerHTML = newTodoValue;
+  newTodoTrash.className = "fa fa-trash-o delete";
+  newTodoLi.append(newTodoTitleSpan, newTodoTrash);
+  todoUlElem.append(newTodoLi);
+
+  console.log(newTodoLi);
+  newTodoTrash.addEventListener("click", function (event) {
+    event.target.parentElement.remove();
   });
+}
+
+addTodoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+inputElem.addEventListener("keydown", function (event) {
+  let newTodoValue = event.target.value.trim();
+  if (event.keyCode === 13) {
+    if (newTodoValue) {
+      addNewTodo(newTodoValue);
+      inputElem.value = "";
+    }
+  }
 });
