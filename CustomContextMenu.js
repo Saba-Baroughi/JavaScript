3d08c1c93422fe86ed8c0dfbf3a93b71ae5e8159
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-10-28 18:12:40 +0100

    CustomContextMenu

---
+++ CustomContextMenu.js
@@ -0,0 +1,26 @@
+let contextMenu = document.getElementById("contextMenu");
+function contextHandler(event) {
+  event.preventDefault();
+  if (contextMenu.style.display === "none") {
+    contextMenu.style.display = "block";
+    contextMenu.style.top = event.pageY + "px";
+    contextMenu.style.left = event.pageX = "px";
+  } else {
+    // when it appeae even if I right clicked another
+    // places still open with different pageX and pageY
+    contextMenu.style.top = event.pageY + "px";
+    contextMenu.style.left = event.pageX + "px";
+  }
+}
+function clickContextMenu() {
+  contextMenu.style.display = "none";
+}
+function keyDownHandler(event) {
+  //the keyCode ofScape=27
+  if (event.keyCode === 27) {
+    contextMenu.style.display = "none";
+  }
+}
+document.body.addEventListener("contextmenu", contextHandler);
+document.body.addEventListener("click", clickContextMenu);
+document.body.addEventListener("keydown", keyDownHandler);
--- practiceSheet.js
+++ practiceSheet.js
@@ -1,35 +1,6 @@
-let $ = document;
-let inputElem = $.querySelector("input");
-let todoUlElem = $.querySelector("ul");
-let addTodoForm = $.querySelector(".add");
-
-function addNewTodo(newTodoValue) {
-  let newTodoTrash = $.createElement("i");
-  let newTodoTitleSpan = $.createElement("span");
-  let newTodoLi = $.createElement("li");
-  newTodoLi.className =
-    "list-group-item d-flex justify-content-between align-items-center";
-  newTodoTitleSpan.innerHTML = newTodoValue;
-  newTodoTrash.className = "fa fa-trash-o delete";
-  newTodoLi.append(newTodoTitleSpan, newTodoTrash);
-  todoUlElem.append(newTodoLi);
-
-  console.log(newTodoLi);
-  newTodoTrash.addEventListener("click", function (event) {
-    event.target.parentElement.remove();
-  });
+function clickHandler(event) {
+  console.log(event);
 }
-
-addTodoForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-});
-
-inputElem.addEventListener("keydown", function (event) {
-  let newTodoValue = event.target.value.trim();
-  if (event.keyCode === 13) {
-    if (newTodoValue) {
-      addNewTodo(newTodoValue);
-      inputElem.value = "";
-    }
-  }
-});
+function contextHandler(event) {
+  console.log(event);
+}
